dc095ca38d4bb45f3b1fe8bd677f16e96bfc08d5
enosaf7 <128220498+enosaf7@users.noreply.github.com>
2025-05-31 12:47:58 +0000

    HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -34,8 +34,8 @@ const HeroSection: React.FC = () => {
           <div className="md:w-1/2 relative">
             <div className="bg-brand-orange rounded-full w-72 h-72 md:w-80 md:h-80 lg:w-96 lg:h-96 absolute -z-10 top-4 right-4"></div>
             <img 
-              src= "https://sdmntprwestus3.oaiusercontent.com/files/00000000-bedc-61fd-9efa-99e6161a0c1c/raw?se=2025-05-15T03%3A47%3A03Z&sp=r&sv=2024-08-04&sr=b&scid=00000000-0000-0000-0000-000000000000&skoid=7399a3a4-0259-4d43-bcd6-a56ceeb4c28b&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-05-15T02%3A22%3A24Z&ske=2025-05-16T02%3A22%3A24Z&sks=b&skv=2024-08-04&sig=K/8UoU2/auIb7sJokA78V3RwJj0Nb1gQpHuOCi3GYOQ%3D" 
-              alt="JE's Palace Executive Shoe" 
+              src= "https://imgur.com/ZXmVZUI" 
+              alt="JE's Palace logo" 
               className="w-full h-auto rounded-lg shadow-lg"
             />
           </div>
